14390f0db7e5a5289ed4692a7b81cad8eba5a02c
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-11-24 18:22:55 +0100

    Add NeighbourCount implementation

--- src/main/java/pl/put/idss/hadoop/simpletasks/NeighbourCount.java
+++ src/main/java/pl/put/idss/hadoop/simpletasks/NeighbourCount.java
@@ -2,7 +2,6 @@ package pl.put.idss.hadoop.simpletasks;
 
 import java.io.IOException;
 import java.util.HashSet;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
@@ -13,7 +12,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class NeighbourCount {
@@ -30,23 +28,43 @@ public class NeighbourCount {
 		Job job = new Job(conf, "neighbourcount");
 		job.setJarByClass(NeighbourCount.class);
 
+        job.setMapperClass(NeighbourMapper.class);
+        //job.setCombinerClass(NeighbourReducer.class);
+        job.setReducerClass(NeighbourReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 
 	}
 
-	public class NeigbourMapper extends Mapper<Object, Text, Text, HashSet<Text>>{
+	public class NeighbourMapper extends Mapper<Object, Text, Text, HashSet<Text>>{
 
-		private Text word = new Text();
+        private Text createNewText(String text){
+            return new Text(text);
+        }
 
 		@Override
 		protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
 			String[] tokens = value.toString().replaceAll("[^a-zA-Z śćóżźąęłĄĘŚĆŹŻŁ]", "").split(" ");
-			for(String token : tokens) {
-				word.set(token);
-				//context.write(word,ONE);
-			}
 
+            for (int i = 0; i<tokens.length; i++)
+            {
+                HashSet<Text> neighbours = new HashSet<>();
+
+                if(i-1 > -1)
+                    neighbours.add(createNewText(tokens[i-1]));
+
+                if(i+1 < tokens.length)
+                    neighbours.add(createNewText(tokens[i+1]));
+
+                context.write(createNewText(tokens[i]), neighbours);
+            }
 		}
 	}
 
@@ -54,7 +72,13 @@ public class NeighbourCount {
 
 		@Override
 		protected void reduce(Text key, Iterable<HashSet<Text>> values, Context context) throws IOException, InterruptedException {
-			super.reduce(key, values, context);
+
+            HashSet<Text> mergedSet = new HashSet<>();
+
+            for (HashSet<Text> neighboursSet : values)
+                mergedSet.addAll(neighboursSet);
+
+            context.write(key, new IntWritable(mergedSet.size()));
 		}
 	}
 
