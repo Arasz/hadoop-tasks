87cba0acc1dc71cb27b9b0f3e17c7743cbbd06de
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-11-24 18:08:47 +0100

    Add new map reduce class

--- src/main/java/pl/put/idss/hadoop/simpletasks/NeighbourCount.java
+++ src/main/java/pl/put/idss/hadoop/simpletasks/NeighbourCount.java
@@ -1,6 +1,7 @@
 package pl.put.idss.hadoop.simpletasks;
 
 import java.io.IOException;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
@@ -31,6 +32,30 @@ public class NeighbourCount {
 
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 
+	}
+
+	public class NeigbourMapper extends Mapper<Object, Text, Text, HashSet<Text>>{
+
+		private Text word = new Text();
+
+		@Override
+		protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+
+			String[] tokens = value.toString().replaceAll("[^a-zA-Z śćóżźąęłĄĘŚĆŹŻŁ]", "").split(" ");
+			for(String token : tokens) {
+				word.set(token);
+				//context.write(word,ONE);
+			}
+
+		}
+	}
+
+	public class NeighbourReducer extends Reducer<Text, HashSet<Text>, Text, IntWritable>{
+
+		@Override
+		protected void reduce(Text key, Iterable<HashSet<Text>> values, Context context) throws IOException, InterruptedException {
+			super.reduce(key, values, context);
+		}
 	}
 
 }
