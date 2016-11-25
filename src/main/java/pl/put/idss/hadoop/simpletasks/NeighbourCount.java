2bc3f69741db8996f9a1b8ce3efc118b8ea97b58
Arasz <rafal.araszkiewicz@student.put.poznan.pl>
2016-11-25 12:30:25 +0100

    Fix and complete neighbours task implementation

--- src/main/java/pl/put/idss/hadoop/simpletasks/NeighbourCount.java
+++ src/main/java/pl/put/idss/hadoop/simpletasks/NeighbourCount.java
@@ -1,12 +1,15 @@
 package pl.put.idss.hadoop.simpletasks;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.HashSet;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -29,11 +32,12 @@ public class NeighbourCount {
 		job.setJarByClass(NeighbourCount.class);
 
         job.setMapperClass(NeighbourMapper.class);
-        //job.setCombinerClass(NeighbourReducer.class);
+        //job.setCombinerClass(NeighbourCollector.class);
         job.setReducerClass(NeighbourReducer.class);
 
+
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
+        job.setOutputValueClass(TextArrayWritable.class);
 
         FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
         FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
@@ -42,14 +46,45 @@ public class NeighbourCount {
 
 	}
 
-	public class NeighbourMapper extends Mapper<Object, Text, Text, HashSet<Text>>{
+	public static class TextArrayWritable extends ArrayWritable{
+
+        public TextArrayWritable() {
+            super(Text.class);
+        }
+
+        public TextArrayWritable(Text[] values) {
+            super(Text.class, values);
+        }
+
+        public TextArrayWritable(Collection<Text> values) {
+            super(Text.class, values.toArray(new Text[values.size()]));
+        }
+
+        public TextArrayWritable(String[] strings) {
+            super(strings);
+        }
+
+        @Override
+        public Text[] get() {
+            return (Text[]) super.get();
+        }
+    }
+
+    private static  TextArrayWritable convertToWritableArray(HashSet<Text> hashSet)
+    {
+        return new TextArrayWritable(hashSet);
+    }
+
+	public static class NeighbourMapper extends Mapper<Object, Text, Text, TextArrayWritable>{
 
         private Text createNewText(String text){
             return new Text(text);
         }
 
+
+
 		@Override
-		protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 
 			String[] tokens = value.toString().replaceAll("[^a-zA-Z śćóżźąęłĄĘŚĆŹŻŁ]", "").split(" ");
 
@@ -63,20 +98,38 @@ public class NeighbourCount {
                 if(i+1 < tokens.length)
                     neighbours.add(createNewText(tokens[i+1]));
 
-                context.write(createNewText(tokens[i]), neighbours);
+                context.write(createNewText(tokens[i]), convertToWritableArray(neighbours));
             }
 		}
 	}
 
-	public class NeighbourReducer extends Reducer<Text, HashSet<Text>, Text, IntWritable>{
-
+	public static class NeighbourCollector extends Reducer<Text, TextArrayWritable, Text, TextArrayWritable>
+	{
 		@Override
-		protected void reduce(Text key, Iterable<HashSet<Text>> values, Context context) throws IOException, InterruptedException {
-
+		public void reduce(Text key, Iterable<TextArrayWritable> values, Context context) throws IOException, InterruptedException {
             HashSet<Text> mergedSet = new HashSet<>();
 
-            for (HashSet<Text> neighboursSet : values)
-                mergedSet.addAll(neighboursSet);
+            for (ArrayWritable neighboursSet : values)
+                for (String text : neighboursSet.toStrings())
+                    mergedSet.add(new Text(text));
+
+			context.write(key, convertToWritableArray(mergedSet));
+		}
+	}
+
+	public static class NeighbourReducer extends Reducer<Text, TextArrayWritable, Text, IntWritable>{
+
+		@Override
+		public void reduce(Text key, Iterable<TextArrayWritable> values, Context context) throws IOException, InterruptedException {
+
+            HashSet<String> mergedSet = new HashSet<>();
+
+            for (TextArrayWritable neighboursSet : values)
+                for (String text : neighboursSet.toStrings())
+                {
+                    System.out.println("---->> "+text);
+                    mergedSet.add(text);
+                }
 
             context.write(key, new IntWritable(mergedSet.size()));
 		}
